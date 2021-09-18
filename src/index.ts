c0aafa5a0d075c85702e21e0d8f37f178d33e5ac
tsutorm <oonishi.wasborn.1981@gmail.com>
2021-09-18 15:46:26 +0900

    bumpup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
+import { listenAndServe } from "https://deno.land/std@0.107.0/http/server.ts"
 
-let port = parseInt(Deno.env.get("PORT") ?? "8000")
-const s = serve({ port });
+const port = parseInt(Deno.env.get("PORT") ?? "8000")
 
-console.log(`http://localhost:${port}/`);
+listenAndServe(`:${port}`, () => new Response("Choo Choo! Welcome to your Deno app\n"))
 
-for await (const req of s) {
-  req.respond({ body: "Choo Choo! Welcome to your Deno app\n" });
-}
+console.log(`http://localhost:${port}/`)

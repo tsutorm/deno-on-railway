72b67fef43e7788102cdf0da2731859254fc3638
onishi.tsutomu <onishi.tsutomu@a-tm.co.jp>
2021-09-30 14:17:50 +0900

    hogehoge

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { listenAndServe, bold, yellow } from "../deps.ts"
 const port = parseInt(Deno.env.get("PORT") ?? "8000")
 
-listenAndServe(`:${port}`, () => new Response("Choo Choo! Welcome to your Deno app\n"))
+listenAndServe(`:${port}`, () => new Response("Choo Choo! Welcome to your Deno app. Try it!!!\n"))
 
 console.log(bold("Start HTTP server on ") + yellow(`http://localhost:${port}/`))
